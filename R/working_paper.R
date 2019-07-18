dfdf6e984baa2632435193375ad15d1b35ea5c18
kbrevoort <ken@brevoort.com>
2019-07-17 23:39:58 -0400

    Add zotero2r functionality

--- R/working_paper.R
+++ R/working_paper.R
@@ -14,5 +14,36 @@ working_paper <- function(...) {
 
   ret_val$inherits <- 'pdf_book'
 
+  # Check if zotero2r is to be used.  If so, create the bibliography file.
+  yaml_front_matter <- get_from_parent_frame('yaml_front_matter')
+  if ('zotero2r' %in% tolower(names(yaml_front_matter))) {
+    if (!is.logical(yaml_front_matter$zotero2r) | yaml_front_matter$zotero2r != FALSE) {
+      rmd_file <- get_from_parent_frame('original_input')
+      zotero2r::create_bibliography(rmd_file)
+    }
+  }
+  #ret_val$pre_processor <- test_function(metadata, input, output, clean, verbose)
+
   ret_val
+}
+
+#' Get from Parent Frame
+#'
+#' Searches parent frames to find a specified variable.
+#' @param x Scalar character vector giving the name of the variable to be searched
+#' for.
+#' @importFrom rlang env_names env_get
+get_from_parent_frame <- function(x) {
+  all_frames <- sys.frames()
+  for (i in all_frames) {
+    if (x %in% rlang::env_names(i)) {
+      return(rlang::env_get(env = i, nm = x))
+    }
+  }
+
+  stop(paste0('Could not locate ', x))
+}
+
+test_function <- function(metadata, input, output, clean, verbose) {
+  browser()
 }
