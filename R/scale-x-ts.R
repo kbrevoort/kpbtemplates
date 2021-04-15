89efd7f29283186c5f336d9971cdf20943300460
kbrevoort <ken@brevoort.com>
2021-04-15 00:26:28 -0400

    Add documentation and streamline scale_x_ts
    Adds capability to select odd, even, or all yearly numbering.

--- R/scale-x-ts.R
+++ R/scale-x-ts.R
@@ -1,28 +1,47 @@
+#' Position scales for time series data
+#'
+#' Alters the appearance of the x-axis when plotting time series data.
+#' Can be used with annual, quarterly, and monthly frequency data. For
+#' annual data, tick marks will be drawn at the beginning and ending of
+#' each year with the label for the year centered below that interval.
+#' When used with quarterly data, smaller tick marks are added to show
+#' end of each quarter.  The option for monthly data includes smaller
+#' tick marks for the first and second months of each quarter.
+#' @param freq Character scalar that determines the type of x-axis to
+#' be displayed.  Options are 'annual' (the default), 'quarterly', and
+#' 'monthly'.
+#' @param name Character vector specifying the name of the axis to be
+#' displayed below the axis
+#' @param limits Date vector with two elements giving the minimum and
+#' maximum values to be shown on the x-axis.
+#' @export
 scale_x_ts <- function(freq = 'annual',
+                       show_years = 'even',
                        name = waiver(),
                        limits = NULL,
                        expand = waiver(),
-                       guide = waiver(),
-                       position = 'bottom') {
+                       guide = waiver()) {
 
   freq <- match.arg(freq, c('annual', 'monthly', 'quarterly'))
+  show_years <- match.arg(show_years, c('all', 'odd', 'even'))
 
   list(geom_ts_scale(),
        .scale_x_ts(freq = freq,
+                   show_years = show_years,
                    name = name,
                    limits = limits,
                    expand = expand,
                    guide = guide,
-                   position = position))
+                   position = 'bottom'))
 }
 
 .scale_x_ts <- function(freq,
+                        show_years,
                         name = waiver(),
                         limits = NULL,
                         expand = waiver(),
                         guide = waiver(),
                         position = 'bottom') {
-
 
   sc <- continuous_scale(aesthetics = c('x', 'xmin', 'xmax', 'xend'),  # was aesthetics
                          scale_name = 'date', # was name
@@ -38,6 +57,7 @@ scale_x_ts <- function(freq = 'annual',
                          position = position,
                          super = ScaleContinuousQuarter)
   sc$set_freq(freq)
+  sc$set_sy(show_years)
   sc
 }
 
@@ -48,19 +68,28 @@ is_odd <- function(x) {
 ScaleContinuousQuarter <- ggproto(`_class` = "ScaleContinuousQuarter",
                                   `_inherit` = ggplot2::ScaleContinuousDate,
                                   freq = 'annual',
-                                  set_freq = function(self, x) {
-                                    self$freq <- x
-                                  },
+                                  show_years = 'even',
+                                  set_sy = function(self, x) self$show_years <- x,
+                                  set_freq = function(self, x) self$freq <- x,
                                   get_breaks = function(self, limits = self$get_limits()) {
+                                    end_points <- as.Date(limits, origin = '1970-01-01')
                                     # This call should always use freq = 'annual'
-                                    get_date_range(limits, use_mid = TRUE, freq = 'annual')
+                                    breaks <- get_date_range(limits, use_mid = TRUE, freq = 'annual')
+                                    breaks <- breaks[range2date(breaks) >= end_points[1L]]
+                                    breaks <- breaks[as.numeric(range2date(breaks)) + 365L < limits[2L]]
+                                    breaks
                                   },
                                   get_labels = function(self, breaks = self$get_breaks()) {
                                     if (is.null(breaks))
                                       return(NULL)
 
                                     my_years <- extract_year(breaks)
-                                    ifelse(is_odd(my_years), '', as.character(my_years))
+                                    if (self$show_years == 'odd') {
+                                      ret_val <- ifelse(is_odd(my_years), as.character(my_years), '')
+                                    } else if (self$show_years == 'even') {
+                                      ret_val <- ifelse(is_odd(my_years), '', as.character(my_years))
+                                    } else ret_val <- my_years
+                                    ret_val
                                   }
 )
 
@@ -84,8 +113,8 @@ draw_ts_scale <- function(data, panel_params, coord) {
 
   grid::gTree(children = grid::gList(
     make_single_polyline(filter(coords, .freq == 'annual'), 'black', 1L),
-    if ('quarterly' %in% to_process) make_single_polyline(filter(coords, .freq == 'quarterly'), 'gray', 1L) else NULL,
-    if ('monthly' %in% to_process) make_single_polyline(filter(coords, .freq == 'monthly'), 'gray', 1L) else NULL))
+    if ('quarterly' %in% to_process) make_single_polyline(subset(coords, .freq == 'quarterly'), 'gray', 1L) else NULL,
+    if ('monthly' %in% to_process) make_single_polyline(subset(coords, .freq == 'monthly'), 'gray', 1L) else NULL))
 }
 
 make_single_polyline <- function(dt, color, width) {
@@ -114,22 +143,19 @@ compute_quarterly_axis_data <- function(freq, dr, yr) {
 
   freq = match.arg(freq, c('annual', 'quarterly', 'monthly'))
   if (freq == 'quarterly') {
-    this_by <- '3 months'
     drop_list <- 1L
     leng = 4 / golden_ratio / 100
   } else if (freq == 'monthly') {
-    this_by <- '1 month'
     drop_list <- c(1L, 4L, 7L, 10L)
     leng <- 4 / golden_ratio / golden_ratio / 100
   } else {
-    this_by <- '1 year'
     drop_list <- c(2L:12L)
     leng <- 4 / 100
   }
 
   py <- seq(from = as.Date(sprintf('%d-01-01', extract_year(dr[1L]))),
             to = as.Date(sprintf('%d-01-01', extract_year(dr[2L]) + 1L)),
-            by = this_by)
+            by = get_by_string(freq))
   py <- py[py >= dr[1L] &
              py <= dr[2L] &
              !extract_month(py) %in% drop_list]
@@ -146,28 +172,31 @@ compute_quarterly_axis_data <- function(freq, dr, yr) {
 get_date_range <- function(x, use_mid = FALSE, freq = 'annual') {
 
   date_range <- as.Date(x, origin = as.Date('1970-01-01'))
-  freq <- match.arg(freq, c('annual', 'quarterly', 'monthly'))
-  if (freq == 'annual') {
-    this_by <- '1 year'
-  } else if (freq == 'quarterly') {
-    this_by <- '3 months'
-  } else this_by <- '1 month'
 
-  py <- seq(from = as.Date(sprintf('%d-%02d-01',
-                                   lubridate::year(date_range[1L]),
-                                   if (use_mid) 7L else 1L)),
-            to = as.Date(sprintf('%d-%02d-01',
-                                 lubridate::year(date_range[2L]),
-                                 if (use_mid) 7L else 1L)),
-            by = this_by)
+  py <- seq(from = range2date(date_range[1L], use_mid),
+            to = range2date(date_range[2L], use_mid),
+            by = get_by_string(freq))
 
   py[py >= date_range[1L] & py <= date_range[2L]]
+}
+
+range2date <- function(r, mid = FALSE) {
+  as.Date(sprintf('%d-%02d-01',
+                  extract_year(r),
+                  if (mid) 7L else 1L))
+}
+
+get_by_string <- function(x) {
+  switch(x,
+         annual = '1 year',
+         quarterly = '3 months',
+         monthly = '1 month')
 }
 
 GeomTSScale <- ggproto("GeomTSScale",
                        Geom,
                        required_aes = c("x", "y"),
-                       default_aes = aes(shape = 19, colour = "black"),
+                       default_aes = aes(colour = "black"),
                        draw_panel = draw_ts_scale)
 
 geom_ts_scale <- function(mapping = NULL, data = NULL, stat = "identity",
